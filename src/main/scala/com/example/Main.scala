5f91bdf20d64540d573b4dff0593e3b29e8f739e
Rodolfo Echemendía <rodobarcaaa@gmail.com>
2022-01-20 00:18:54 -0300

    chore: try to test linter fail

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -3,18 +3,24 @@ package com.example
 import cats.effect.{ExitCode, IO, IOApp}
 import com.typesafe.scalalogging.StrictLogging
 
-object Main extends IOApp with StrictLogging {
+object Main extends IOApp 
+with StrictLogging {
 
-  override def run(args: List[String]): IO[ExitCode] = {
-    val module = MainModule.initialize
+  override def run(args: List[String]): IO[ExitCode] = 
+  
+  {
+              val module =           MainModule.initialize
     module.httpApi.resource
       .use { _ =>
         val baseUrl = module.httpConfig.toUrl("http://")
         logger.info(s"Documentation from $baseUrl/docs")
         logger.info(s"Metrics from $baseUrl/metrics")
         IO.never
+      
+      
+      
       }
-      .as(ExitCode.Success)
+                 .as(ExitCode.Success)
   }
 
 }
